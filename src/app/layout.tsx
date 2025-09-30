824455946bf88b933fe12373c295bc6a35f827f1
aleckshen <aleckshn@gmail.com>
2025-09-30 22:10:55 +1300

    added font and bg colour

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,17 @@
 import "./globals.css";
+import type { ReactNode } from "react";
+import { IBM_Plex_Mono } from "next/font/google";
+
+const plexMono = IBM_Plex_Mono({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"], 
+  variable: "--font-plex-mono",
+});
+
+export const metadata = {
+  title: "My Terminal Portfolio",
+  description: "A terminal-style personal website",
+};
 
 export default function RootLayout({
   children,
@@ -7,8 +20,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
-        
+      <body className={`${plexMono.variable} bg-[#1C1E26] text-[#97E0A6] font-mono`}>
+        {children}
       </body>
     </html>
   );
